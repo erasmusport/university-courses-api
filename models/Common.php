0c71e46f16099ffb9d96eeef3326abc84c6c4c7d
sascoms <sascoms@gmail.com>
2021-07-13 17:56:52 +0300

    + message improvement.

--- models/Common.php
+++ models/Common.php
@@ -8,7 +8,7 @@
 
             if (!in_array($_SERVER['REMOTE_ADDR'], $allowedIpNumbers)) {
                 //throw new Exception('ERR404|Unauthorized access!(' . $_SERVER['REMOTE_ADDR'] . '' . print_r($allowedIpNumbers, true) . ')|' . $_SERVER['REMOTE_ADDR']);
-                throw new Exception('ERR404|Unauthorized access!(' . $_SERVER['REMOTE_ADDR'] . ')|' . $_SERVER['REMOTE_ADDR']);
+                throw new Exception('ERROR 400 | Unauthorized access! (' . $_SERVER['REMOTE_ADDR'] . ') | ' . $_SERVER['REMOTE_ADDR']);
             }
         }
 
